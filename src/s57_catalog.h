1c97c7da76cf1575c825af719ca2e0e830302693
kuhnentrion <kuhnen@trion.gmbh>
2026-08-18 11:36:50 +0200

    Improve S-57 readable data model

--- src/s57_catalog.h
+++ src/s57_catalog.h
@@ -8,14 +8,22 @@
 
 class S57Catalog {
 public:
+  struct ObjectClassInfo {
+    wxString acronym;
+    wxString name;
+  };
+
   bool Load(const wxString &sharedDataDirectory);
 
   bool IsLoaded() const { return m_loaded; }
   wxString ObjectName(const wxString &acronym) const;
-  wxString FormatAttributes(const wxString &rawAttributes) const;
+  std::vector<ObjectClassInfo> ObjectClasses() const;
+  wxString FormatAttributes(const wxString &rawAttributes,
+                            wxString *technical = nullptr) const;
 
 private:
   struct AttributeDefinition {
+    long code = -1;
     wxString name;
     wxString type;
   };
@@ -25,15 +33,21 @@ private:
   static std::vector<wxString> ParseCsvLine(const wxString &line);
   static wxString Trimmed(wxString value);
   static wxString UppercaseFirst(const wxString &value);
+  static wxString FriendlyLabel(const wxString &acronym,
+                                const wxString &catalogLabel);
+  static wxString FormatDate(const wxString &value);
 
   bool LoadObjectClasses(const wxString &path);
   bool LoadAttributes(const wxString &path);
   bool LoadAttributeDecodes(const wxString &path);
+  bool LoadExpectedInput(const wxString &path);
   wxString DecodeAttributeValue(const wxString &acronym,
-                                const wxString &rawValue) const;
+                                const wxString &rawValue,
+                                bool *decodedAny = nullptr) const;
 
   std::map<wxString, wxString> m_objectNames;
   std::map<wxString, AttributeDefinition> m_attributes;
+  std::map<long, wxString> m_attributeByCode;
   std::map<wxString, DecodeTable> m_decodes;
   bool m_loaded = false;
 };
