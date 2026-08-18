9d996a5ef0c1954fd547b48278dc3fe44d1f1714
kuhnentrion <kuhnen@trion.gmbh>
2026-08-18 08:54:43 +0200

    Add plugin class header

---
+++ src/chartinspector_pi.h
@@ -0,0 +1,36 @@
+#ifndef CHARTINSPECTOR_PI_H
+#define CHARTINSPECTOR_PI_H
+
+#include <wx/wx.h>
+
+#include "ocpn_plugin.h"
+
+class ChartInspectorPi : public opencpn_plugin_118 {
+public:
+  explicit ChartInspectorPi(void *ppimgr);
+  ~ChartInspectorPi() override = default;
+
+  int Init() override;
+  bool DeInit() override;
+
+  int GetAPIVersionMajor() override;
+  int GetAPIVersionMinor() override;
+  int GetPlugInVersionMajor() override;
+  int GetPlugInVersionMinor() override;
+
+  wxBitmap *GetPlugInBitmap() override;
+  wxString GetCommonName() override;
+  wxString GetShortDescription() override;
+  wxString GetLongDescription() override;
+
+  void SetCursorLatLon(double lat, double lon) override;
+  bool MouseEventHook(wxMouseEvent &event) override;
+  bool RenderOverlay(wxDC &dc, PlugIn_ViewPort *vp) override;
+
+private:
+  double m_cursorLat = 0.0;
+  double m_cursorLon = 0.0;
+  bool m_hasCursorPosition = false;
+};
+
+#endif  // CHARTINSPECTOR_PI_H
