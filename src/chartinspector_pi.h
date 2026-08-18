444f1e716f9308f856cc18bb8c6c52aea03017da
kuhnentrion <kuhnen@trion.gmbh>
2026-08-18 09:29:17 +0200

    Capture vector chart object callbacks

--- src/chartinspector_pi.h
+++ src/chartinspector_pi.h
@@ -25,6 +25,9 @@ public:
 
   void SetCursorLatLon(double lat, double lon) override;
   bool MouseEventHook(wxMouseEvent &event) override;
+  void SendVectorChartObjectInfo(wxString &chart, wxString &feature,
+                                 wxString &objname, double lat, double lon,
+                                 double scale, int nativescale) override;
   bool RenderOverlay(wxDC &dc, PlugIn_ViewPort *vp) override;
   bool RenderGLOverlayMultiCanvas(wxGLContext *pcontext, PlugIn_ViewPort *vp,
                                   int canvasIndex, int priority = -1) override;
@@ -36,6 +39,15 @@ private:
   double m_cursorLon = 0.0;
   bool m_hasMousePosition = false;
   bool m_hasCursorPosition = false;
+
+  wxString m_lastChart;
+  wxString m_lastFeature;
+  wxString m_lastObjectName;
+  double m_lastObjectLat = 0.0;
+  double m_lastObjectLon = 0.0;
+  double m_lastObjectScale = 0.0;
+  int m_lastObjectNativeScale = 0;
+  bool m_hasVectorObject = false;
 };
 
 #endif  // CHARTINSPECTOR_PI_H
