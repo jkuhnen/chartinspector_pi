cbe10bdcc5c2095e05f71ecd9a36791da55980f2
Johannes Kuhnen <johanneskuhnen@gmx.de>
2026-08-24 11:19:18 +0200

    Add Maritime UI demo plugin interface

---
+++ demo/maritimeui_pi/include/maritimeui_pi.h
@@ -0,0 +1,46 @@
+#ifndef MARITIMEUI_PI_H
+#define MARITIMEUI_PI_H
+
+#include <wx/wx.h>
+#include "ocpn_plugin.h"
+
+class MaritimeUiPi : public opencpn_plugin_118 {
+public:
+  explicit MaritimeUiPi(void *ppimgr);
+  ~MaritimeUiPi() override = default;
+
+  int Init() override;
+  bool DeInit() override;
+
+  int GetAPIVersionMajor() override;
+  int GetAPIVersionMinor() override;
+  int GetPlugInVersionMajor() override;
+  int GetPlugInVersionMinor() override;
+  int GetToolbarToolCount() override;
+
+  wxBitmap *GetPlugInBitmap() override;
+  wxString GetCommonName() override;
+  wxString GetShortDescription() override;
+  wxString GetLongDescription() override;
+
+  void OnToolbarToolCallback(int id) override;
+  void SetColorScheme(PI_ColorScheme cs) override;
+
+private:
+  void BuildToolbarBitmap();
+  void EnsureWindow();
+  void ApplyTheme();
+  void ToggleWindow();
+  wxPanel *CreateCard(wxWindow *parent, const wxString &eyebrow,
+                      const wxString &title, const wxString &body);
+  wxPanel *CreateStatusRow(wxWindow *parent, const wxString &label,
+                           const wxString &value, int semantic);
+
+  int m_toolbarId = -1;
+  wxBitmap m_pluginBitmap;
+  wxBitmap m_toolbarBitmap;
+  wxFrame *m_window = nullptr;
+  PI_ColorScheme m_colorScheme = PI_GLOBAL_COLOR_SCHEME_DAY;
+};
+
+#endif
